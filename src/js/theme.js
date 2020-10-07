ff23b549326b2be4db3c89f9973692585f705c27
olena-kharchenko <harchenko.es1990@gmail.com>
2020-10-07 13:13:58 +0300

    feat: add theme switching

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,3 +3,29 @@ const theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+const bodyEl = document.querySelector('body');
+const checkBoxEl = document.querySelector('#theme-switch-toggle');
+const STORAGE_KEY = 'lightTheme';
+const savedTheme = localStorage.getItem(STORAGE_KEY);
+
+bodyEl.classList.add(theme.LIGHT);
+checkBoxEl.addEventListener('change', onCheckBoxChange);
+
+if (savedTheme === '') {
+  bodyEl.classList.add(theme.LIGHT);
+}
+
+if (savedTheme === 'false') {
+  bodyEl.classList.remove(theme.LIGHT);
+  bodyEl.classList.add(theme.DARK);
+  checkBoxEl.checked = true;
+}
+
+function onCheckBoxChange(evt) {
+  bodyEl.classList.toggle(theme.LIGHT);
+  bodyEl.classList.toggle(theme.DARK);
+
+  const isLight = bodyEl.classList.contains(theme.LIGHT);
+  localStorage.setItem(STORAGE_KEY, isLight);
+}
